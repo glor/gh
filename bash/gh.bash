e6ff5a5fd2118054650cfdb0d0eaecf8c038265d
glor <glor@gitlab.com>
2016-08-19 13:32:23 +0200

    Add URL parsing to bash; Change to https

--- bash/gh.bash
+++ bash/gh.bash
@@ -1,18 +1,29 @@
+#!/bin/bash
 GH_BASE_DIR=${GH_BASE_DIR:-$HOME/src}
 function gh() {
-  if [[ $# -ne 2 ]]; then
-    echo "USAGE: gh [user] [repo]"
-    return
+  URL=$1
+  # one argument and a matching url
+  if [[ $# -eq 1 ]] && [[ "$1" == https://github.com/*/* ]];
+  then
+    # cut down url and remove /; remove ".git" if it is at the end
+	URL=$(echo ${URL:19} | tr "/" " " | sed "s/.git$//g" )
+	user=$(echo $URL | cut -f1 -d" ")
+	repo=$(echo $URL | cut -f2 -d" ")
+  else
+  	if [[ $# -ne 2 ]]; then
+      echo "USAGE: gh [user] [repo]"
+      echo "   or: gh [url]"
+      return
+    fi
+    user=$1
+    repo=$2
   fi
-
-  user=$1
-  repo=$2
-
+  
   user_path=$GH_BASE_DIR/github.com/$user
   local_path=$user_path/$repo
 
   if [[ ! -d $local_path ]]; then
-    git clone git@github.com:$user/$repo.git $local_path
+    git clone https://github.com/$user/$repo.git $local_path
   fi
 
   # If git exited uncleanly, clean up the created user directory (if exists)
